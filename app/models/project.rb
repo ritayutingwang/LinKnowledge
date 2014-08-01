5cb6e9e412006c3662621a711f2510ad3f5d67c4
ritayutingwang <dennyreave@gmail.com>
2014-08-01 16:57:49 +0800

    def time_left method

--- app/models/project.rb
+++ app/models/project.rb
@@ -18,4 +18,25 @@
 class Project < ActiveRecord::Base
   has_many :backings
   include BelongsToUser
+
+  def due_time(zone)
+    (self.created_at + self.duration.days).in_time_zone(zone).to_datetime
+  end
+
+  def time_left
+    secs = self.duration.days - (Time.now.utc - self.created_at).to_int
+    mins = secs / 60
+    hours = mins / 60
+    days = hours / 24
+
+    if days > 0
+      "#{days} days and #{hours % 24} hours"
+    elsif hours > 0
+      "#{hours} hours and #{mins % 60} minutes"
+    elsif mins > 0
+      "#{mins} minutes and #{secs % 60} seconds"
+    elsif secs >= 0
+      "#{secs} seconds"
+    end
+  end
 end
