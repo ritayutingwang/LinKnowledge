de72a495b4938be453f0a524a80f99721ca24d81
Brian Hu <billy170916@gmail.com>
2014-08-04 11:53:32 +0800

    fixed super stupid typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,7 @@ class User < ActiveRecord::Base
     where(auth.slice(:provider, :uid)).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.first_name = auth.info.first_nmae
+      user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
       user.image = auth.info.image
     end
