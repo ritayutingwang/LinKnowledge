2dc9d84018ebe065b856df1ac7c319e2b7080803
Rita Wang <dennyreave@gmail.com>
2014-10-06 19:41:13 +0800

    User profile edit is ready

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(
-      :nickname, :usingIdenticon
+      :nickname, :usingFbProfile
     )
   end
 end
