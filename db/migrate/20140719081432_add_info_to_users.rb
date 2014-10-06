a24dca0353d6c137b327af37e7bb3d516d876e25
Brian Hu <billy170916@gmail.com>
2014-10-06 09:23:52 +0800

    updated user model for the campaign

--- db/migrate/20140719081432_add_info_to_users.rb
+++ db/migrate/20140719081432_add_info_to_users.rb
@@ -2,6 +2,8 @@ class AddInfoToUsers < ActiveRecord::Migration
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
-    add_column :users, :image, :string
+    add_column :users, :nickname, :string
+    add_column :users, :facebook_profile, :string
+    add_column :users, :usingFbProfile, :boolean, default: true
   end
 end
